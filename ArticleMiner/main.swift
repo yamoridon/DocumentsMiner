2eed7e66689a4d4164ce3d003a19605927b1acef
Kazuki Ohara <kazuki.ohara@gmail.com>
2019-01-16 23:08:44 +0900

    Initial Commit

---
+++ ArticleMiner/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ArticleMiner
+//
+//  Created by Kazuki Ohara on 2019/01/16.
+//  Copyright © 2019 Kazuki Ohara. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
